d2858645418e2ff0fe787e62bf4e8a7f9184091b
Chris <chrisdrobinsonrules@gmail.com>
2019-05-21 17:33:33 -0400

    Complete

--- src/main/java/easyModerator/Listener.java
+++ src/main/java/easyModerator/Listener.java
@@ -1,6 +1,7 @@
 package easyModerator;
 
 import bot.configuration.ConfigManager;
+import net.dv8tion.jda.core.entities.Message;
 import net.dv8tion.jda.core.entities.MessageReaction;
 import net.dv8tion.jda.core.events.message.react.MessageReactionAddEvent;
 import net.dv8tion.jda.core.hooks.ListenerAdapter;
@@ -8,16 +9,21 @@ import net.dv8tion.jda.core.hooks.ListenerAdapter;
 public class Listener extends ListenerAdapter {
 
     private ConfigManager cm = new ConfigManager();
-    private final String deletionEmote = ":x:";
+    private static final String deletionEmote = "❌";
     //This should work if you use a custom emote or a unicode emoji
     //Custom emote: :leagueOfLegendsLogo:
-    //Emoji: 😄 or :smiley:
+    //Emoji: 😄 or ❌ or :smiley:
 
     @Override
     public void onMessageReactionAdd(MessageReactionAddEvent event) {
         if (event.getMember().getUser().isBot()) return;
         if (!event.getGuild().getId().equals(cm.getGuildId())) return;
         MessageReaction.ReactionEmote reactionEmote = event.getReactionEmote();
-        System.out.println(reactionEmote.toString());
+        if (reactionEmote.getName().equals(deletionEmote)) {
+            String messageID = event.getMessageId();
+            Message message = event.getChannel().getMessageById(messageID).complete();
+            message.delete().queue();
+            event.getGuild().getTextChannelById("337763599835594752").sendMessage(message).queue();
+        }
     }
 }
